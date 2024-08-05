2bbae10a8857623f2248e6d19a2335608b562486
madhurajangale <142075673+madhurajangale@users.noreply.github.com>
2024-08-05 22:57:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Admin from './Components/admin';
+function App() {
+  return (
+    <div className="App">
+      <Admin></Admin>
+    </div>
+  );
+}
+
+export default App;
